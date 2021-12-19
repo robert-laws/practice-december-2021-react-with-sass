b2d17499e6766d432d66ee62aaf3df35f64b1ca7
Robert Laws <roblaws@me.com>
2021-12-19 20:28:45 +0300

    setup react app with basic sass

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './sass/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>App</h1>
+        <h2>Subtitle</h2>
       </header>
     </div>
   );
