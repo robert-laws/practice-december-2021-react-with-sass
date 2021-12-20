0b3dc1a9230db7476a3097dd2573c9d638e25df9
Robert Laws <roblaws@me.com>
2021-12-20 12:10:55 +0300

    clean up app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import './sass/App.scss';
-import Button from './Button';
-import NewButton from './NewButton';
-import styled from 'styled-components';
-import StyledParagraph from './StyledParagraph';
-
-const StyledDiv = styled.div`
-  background-color: red;
-  color: white;
-  padding: 20px;
-`;
+// import './sass/App.scss';
 
 function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        backgroundColor: 'lightblue',
-      }}
-      className='App'
-    >
-      <header className='App-header'>
-        <h1>App</h1>
-        <h2>Subtitle</h2>
-        <Button>Click Here</Button>
-        <hr />
-        <NewButton>Click Again</NewButton>
-      </header>
-      <StyledDiv>
-        <StyledParagraph>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea,
-          consectetur iure. Quae.
-        </StyledParagraph>
-      </StyledDiv>
+    <div>
+      <h1>App</h1>
     </div>
   );
 }
--- src/StyledParagraph.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const StyledParagraph = styled.p`
-  font-size: 2rem;
-`;
-
-export default StyledParagraph;
